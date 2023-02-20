3892f57f21a1bb0f71424665ceb31e07f93f5a45
ShevchukA <antoshka.vbg@mail.ru>
2023-02-20 11:03:16 +0300

    Set up personalities interactions

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
+const btns = document.querySelectorAll(".card__btn");
+const cardSide1 = document.querySelectorAll(".card__side1");
+const cardSide2 = document.querySelectorAll(".card__side2");
 
+for (const [index, btn] of btns.entries()) {
+  btn.addEventListener("click", () => onClick(btn, index));
+}
+
+function onClick(btn, i) {
+  btn.classList.toggle("card__btn--active");
+  cardSide1[i].classList.toggle("card__side1--turned");
+  cardSide2[i].classList.toggle("card__side2--turned");
+}
